c30789af353e37569e3dae7c910f3684320f8d8e
kristjoc <25754988+kristjoc@users.noreply.github.com>
2025-06-05 11:05:48 +0200

    Bump version to v1.0

--- bible-gateway.el
+++ bible-gateway.el
@@ -6,7 +6,7 @@
 ;; Keywords: convenience comm hypermedia
 ;; Homepage: https://github.com/kristjoc/bible-gateway
 ;; Package-Requires: ((emacs "29.1"))
-;; Package-Version: 0.9
+;; Package-Version: 1.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
